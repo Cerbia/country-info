7c37f6a557f9100a8a17466b6e2fa83013b78d35
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-07-26 23:30:29 +0200

    Add transform-class-properties, refactorization of a google maps object

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -2,118 +2,159 @@
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-/*reorganize code & create 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/*reorganize code & create
 https://hackernoon.com/observer-vs-pub-sub-pattern-50d3b27f838c
 https://gist.github.com/learncodeacademy/777349747d8382bfb722
 */
+//TODO: Classes Map and Countries
 
-var prefix = "https://cryptic-headland-94862.herokuapp.com/";
-var url = 'https://restcountries.eu/rest/v2/name/';
-var countriesButtonContainer = document.querySelector('.countries-container');
-// mustache template
-var templateCountryButton = document.getElementById('template-country').innerHTML;
-var countryInput = document.getElementById('country-name');
-var map;
-var theParentCountryButtonEl;
-var countriesMap = new Map();
-var countriesNames = [];
-var markersArray = [];
+var Marker = function () {
+    function Marker(engine) {
+        _classCallCheck(this, Marker);
 
-document.getElementById('search').addEventListener('click', searchCountries);
+        this.engine = engine;
+        this.markersArray = [];
+    }
 
-countryInput.addEventListener('keyup', function (event) {
+    _createClass(Marker, [{
+        key: 'setUpMarkers',
+        value: function setUpMarkers() {
+            var _iteratorNormalCompletion = true;
+            var _didIteratorError = false;
+            var _iteratorError = undefined;
+
+            try {
+                for (var _iterator = this.engine.countriesMap[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                    var _step$value = _slicedToArray(_step.value, 2),
+                        k = _step$value[0],
+                        v = _step$value[1];
+
+                    this.markersArray.push(new google.maps.Marker({ position: v.latlng, map: engine.map }));
+                }
+            } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+            } finally {
+                try {
+                    if (!_iteratorNormalCompletion && _iterator.return) {
+                        _iterator.return();
+                    }
+                } finally {
+                    if (_didIteratorError) {
+                        throw _iteratorError;
+                    }
+                }
+            }
+        }
+    }, {
+        key: 'cleanUpMarkers',
+        value: function cleanUpMarkers() {
+            this.markersArray.forEach(function (item) {
+                console.log('item' + item);
+                item.setMap(null);
+            });
+            this.markersArray = [];
+        }
+    }]);
+
+    return Marker;
+}();
+
+var Application = function () {
+    function Application() {
+        _classCallCheck(this, Application);
+
+        this.countriesButtonContainer = document.querySelector('.countries-container');
+        this.templateCountryButton = document.getElementById('template-country').innerHTML;
+        this.countryInput = document.getElementById('country-name');
+        this.countriesMap = new Map();
+        this.marker = new Marker(this);
+        this.map = new google.maps.Map(document.getElementById('map'), { zoom: 5, center: { lat: 52, lng: 20 } });
+    }
+
+    _createClass(Application, [{
+        key: 'searchCountries',
+        value: function searchCountries() {
+            var countryName = this.countryInput.value;
+            if (!countryName) countryName = 'Poland';
+
+            fetch(Application.prefix + Application.url + countryName).then(function (response) {
+                return response.json();
+            }).then(showCountriesList).catch(function () {
+                alert("Country not found, type a different name");
+            });
+        }
+    }, {
+        key: 'clearResult',
+        value: function clearResult() {
+            this.countriesNames = [];
+            this.countriesMap.clear();
+            this.marker.cleanUpMarkers();
+            this.countriesButtonContainer.innerHTML = '';
+        }
+    }]);
+
+    return Application;
+}();
+
+Application.prefix = 'https://cryptic-headland-94862.herokuapp.com/';
+Application.url = 'https://restcountries.eu/rest/v2/name/';
+Application.enterKeyCode = 13;
+
+
+var engine = new Application();
+
+document.getElementById('search').addEventListener('click', engine.searchCountries.bind(engine));
+
+engine.countryInput.addEventListener('keyup', function (event) {
+
     event.preventDefault();
-    if (event.keyCode === 13) {
+
+    if (event.keyCode === Application.enterKeyCode) {
         document.getElementById('search').click();
     }
 });
 
-function searchCountries() {
-    var countryName = countryInput.value;
-    if (!countryName) countryName = 'Poland';
-
-    fetch(prefix + url + countryName).then(function (response) {
-        return response.json();
-    }).then(showCountriesList);
-}
-
 function showCountriesList(resp) {
-    //czyszczenie
-    countriesNames = [];
-    countriesMap.clear();
-    cleanUpMarkers();
-    // Czyszczenie buttonów w html
-    countriesButtonContainer.innerHTML = '';
+    engine.clearResult();
 
     resp.forEach(function (item) {
-        countriesMap.set(item.name, { latlng: { lat: Number(item.latlng[0]), lng: Number(item.latlng[1]) }, flag: item.flag, nativeName: item.nativeName, capital: item.capital, currencies: item.currencies, languages: item.languages });
-        countriesNames.push({ name: item.name });
-        //console.log(item.name + 'latlng: {lat: ' + item.latlng[0] + ', lng: ' + item.latlng[1] + '}');
+        engine.countriesMap.set(item.name, {
+            latlng: { lat: Number(item.latlng[0]), lng: Number(item.latlng[1]) },
+            flag: item.flag,
+            nativeName: item.nativeName,
+            capital: item.capital,
+            currencies: item.currencies,
+            languages: item.languages
+        });
+        engine.countriesNames.push({ name: item.name });
     });
 
     generateCountriesList();
-    setUpMarkers();
+    engine.marker.setUpMarkers();
 }
 
 function generateCountriesList() {
-    var generateButtonTemplate = "";
-    Mustache.parse(templateCountryButton);
-    for (var i = 0; i < countriesNames.length; i++) {
-        generateButtonTemplate += Mustache.render(templateCountryButton, countriesNames[i]);
+    var generateButtonTemplate = '';
+    Mustache.parse(engine.templateCountryButton);
+    for (var i = 0; i < engine.countriesNames.length; i++) {
+        generateButtonTemplate += Mustache.render(engine.templateCountryButton, engine.countriesNames[i]);
     }
-    countriesButtonContainer.insertAdjacentHTML('afterbegin', generateButtonTemplate);
+    engine.countriesButtonContainer.insertAdjacentHTML('afterbegin', generateButtonTemplate);
 }
 
-theParentCountryButtonEl = document.querySelector('.countries-container');
+var theParentCountryButtonEl = document.querySelector('.countries-container');
 
 theParentCountryButtonEl.addEventListener('click', centerCountryOnTheMap, false);
 
 function centerCountryOnTheMap(e) {
     if (e.target !== e.currentTarget) {
-        map.setCenter(countriesMap.get(e.target.innerHTML).latlng);
-        map.setZoom(5);
+        engine.map.setCenter(engine.countriesMap.get(e.target.innerHTML).latlng);
+        engine.map.setZoom(5);
     }
     e.stopPropagation();
-}
-
-window.initMap = function () {
-    map = new google.maps.Map(document.getElementById('map'), { zoom: 5, center: { lat: 52, lng: 20 } });
-};
-
-function setUpMarkers() {
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-        for (var _iterator = countriesMap[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var _step$value = _slicedToArray(_step.value, 2),
-                k = _step$value[0],
-                v = _step$value[1];
-
-            //console.log(k, v);
-            markersArray.push(new google.maps.Marker({ position: v.latlng, map: map }));
-        }
-    } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-            }
-        } finally {
-            if (_didIteratorError) {
-                throw _iteratorError;
-            }
-        }
-    }
-}
-
-function cleanUpMarkers() {
-    markersArray.forEach(function (item) {
-        item.setMap(null);
-    });
-    markersArray = [];
-    //console.log(markersArray);
 }
--- js/script.js
+++ js/script.js
@@ -5,114 +5,109 @@ https://gist.github.com/learncodeacademy/777349747d8382bfb722
 //TODO: Classes Map and Countries
 
 class Marker {
-  constructor(engine) {
-    this.engine = engine
-    this.markersArray = []
-  }
-
-  setUpMarkers() {
-    for (let [k, v] of this.engine.countriesMap) {
-      this.markersArray.push(new google.maps.Marker({position: v.latlng, map: map}))
+    constructor(engine) {
+        this.engine = engine; 
+        this.markersArray = [];
     }
-  }
 
-  cleanUpMarkers() {
-    this.markersArray.forEach(function (item) {
-      item.setMap(null)
-    })
-    this.markersArray = []
-  }
+    setUpMarkers() {
+        for (let [k, v] of this.engine.countriesMap) {
+            this.markersArray.push(new google.maps.Marker({position: v.latlng, map: engine.map}))
+        }
+    }
+
+    cleanUpMarkers() {
+        this.markersArray.forEach(function (item) {
+            console.log('item' + item);
+            item.setMap(null);
+        });
+        this.markersArray = [];
+    }
 }
 
 class Application {
-  static prefix = 'https://cryptic-headland-94862.herokuapp.com/'
+    static prefix = 'https://cryptic-headland-94862.herokuapp.com/';
+    static url = 'https://restcountries.eu/rest/v2/name/';
+    static enterKeyCode = 13;
 
-  constructor() {
-    this.countriesButtonContainer = document.querySelector('.countries-container')
-    this.templateCountryButton = document.getElementById('template-country').innerHTML
-    this.countryInput = document.getElementById('country-name')
-    this.countriesMap = new Map()
-    this.marker = new Marker(this)
-  }
+    constructor() {
+        this.countriesButtonContainer = document.querySelector('.countries-container');
+        this.templateCountryButton = document.getElementById('template-country').innerHTML;
+        this.countryInput = document.getElementById('country-name');
+        this.countriesMap = new Map();
+        this.marker = new Marker(this);
+        this.map = new google.maps.Map(document.getElementById('map'), {zoom: 5, center: {lat: 52, lng: 20}});
+    }
 
-  searchCountries() {
-    var countryName = this.countryInput.value
-    if (!countryName) countryName = 'Poland'
+    searchCountries() {
+        let countryName = this.countryInput.value;
+        if (!countryName) countryName = 'Poland';
 
-    fetch(Application.prefix + url + countryName)
-      .then(response => response.json())
-      .then(showCountriesList)
-  }
+        fetch(Application.prefix + Application.url + countryName)
+            .then(response => response.json())
+            .then(showCountriesList)
+            .catch(() => {
+                alert("Country not found, type a different name");
+            });
+    }
 
-  clearResult() {
-    this.countriesNames = []
-    this.countriesMap.clear()
-    this.marker.cleanUpMarkers()
-  }
-
+    clearResult() {
+        this.countriesNames = [];
+        this.countriesMap.clear();
+        this.marker.cleanUpMarkers();
+        this.countriesButtonContainer.innerHTML = '';
+    }
 }
 
+const engine = new Application();
 
-const url = 'https://restcountries.eu/rest/v2/name/'
-
-
-const engine = new Application()
-
-document.getElementById('search').addEventListener('click', engine.searchCountries.bind(engine))
+document.getElementById('search').addEventListener('click', engine.searchCountries.bind(engine));
 
 engine.countryInput.addEventListener('keyup', function (event) {
-  event.preventDefault()
-  if (event.keyCode === 13) {
-    document.getElementById('search').click()
-  }
-})
+    
+    event.preventDefault();
 
+    if (event.keyCode === Application.enterKeyCode) {
+        document.getElementById('search').click();
+    }
+});
 
 function showCountriesList(resp) {
-  engine.clearResult();
+    engine.clearResult();
 
-  // Czyszczenie buttonów w html
-  engine.countriesButtonContainer.innerHTML = ''
-
-  resp.forEach(function (item) {
-    engine.countriesMap.set(item.name, {
-      latlng: {lat: Number(item.latlng[0]), lng: Number(item.latlng[1])},
-      flag: item.flag,
-      nativeName: item.nativeName,
-      capital: item.capital,
-      currencies: item.currencies,
-      languages: item.languages,
+    resp.forEach(function (item) {
+        engine.countriesMap.set(item.name, {
+            latlng: {lat: Number(item.latlng[0]), lng: Number(item.latlng[1])},
+            flag: item.flag,
+            nativeName: item.nativeName,
+            capital: item.capital,
+            currencies: item.currencies,
+            languages: item.languages,
+        })
+        engine.countriesNames.push({name: item.name});
     })
-    engine.countriesNames.push({name: item.name})
-  })
 
-  generateCountriesList()
-  engine.marker.setUpMarkers()
+    generateCountriesList();
+    engine.marker.setUpMarkers();
 }
 
 function generateCountriesList() {
-  var generateButtonTemplate = ''
-  Mustache.parse(engine.templateCountryButton)
-  for (var i = 0; i < engine.countriesNames.length; i++) {
-    generateButtonTemplate += Mustache.render(engine.templateCountryButton, engine.countriesNames[i])
-  }
-  engine.countriesButtonContainer.insertAdjacentHTML('afterbegin', generateButtonTemplate)
+    let generateButtonTemplate = '';
+    Mustache.parse(engine.templateCountryButton);
+    for (let i = 0; i < engine.countriesNames.length; i++) {
+        generateButtonTemplate += Mustache.render(engine.templateCountryButton, engine.countriesNames[i]);
+    }
+    engine.countriesButtonContainer.insertAdjacentHTML('afterbegin', generateButtonTemplate);
 }
 
-const theParentCountryButtonEl = document.querySelector('.countries-container')
+const theParentCountryButtonEl = document.querySelector('.countries-container');
 
-theParentCountryButtonEl.addEventListener('click', centerCountryOnTheMap, false)
+theParentCountryButtonEl.addEventListener('click', centerCountryOnTheMap, false);
 
 function centerCountryOnTheMap(e) {
-  if (e.target !== e.currentTarget) {
-    map.setCenter(engine.countriesMap.get(e.target.innerHTML).latlng)
-    map.setZoom(5)
-  }
-  e.stopPropagation()
-}
-
-window.initMap = function () {
-  map = new google.maps.Map(
-    document.getElementById('map'), {zoom: 5, center: {lat: 52, lng: 20}},
-  )
+    if (e.target !== e.currentTarget) {
+        engine.map.setCenter(engine.countriesMap.get(e.target.innerHTML).latlng);
+        engine.map.setZoom(5);
+    }
+    e.stopPropagation();
 }
